1d9669967cb34ecf45d64a6220286877cf14e125
eg204115 <nethmipoornima2020@gmail.com>
2025-07-24 19:50:37 +0530

    Modified logging in both file and db

--- SecureChatApp_backend/models/AuditLogSchema .js
+++ SecureChatApp_backend/models/AuditLogSchema .js
@@ -1,7 +1,9 @@
+const mongoose = require('mongoose');
+
 const auditLogSchema = new mongoose.Schema({
-  eventType: { type: String, required: true }, // e.g., AUTH_SUCCESS
+  eventType: { type: String, required: true },
   username: { type: String },
-  timestamp: { type: Date, default: Date.now },
+  timestamp: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model("AuditLog", auditLogSchema);
